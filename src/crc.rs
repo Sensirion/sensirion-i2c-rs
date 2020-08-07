d7114270674b9ecf6b4291d5464507f9b8337602
Raphael Nestler <raphael.nestler@sensirion.com>
2020-08-07 18:26:35 +0200

    Rename crc8 to calculate and add validate function
    crc::calculate and crc::validate are nicer than crc::crc8

--- src/crc.rs
+++ src/crc.rs
@@ -1,5 +1,5 @@
 /// Calculate the CRC8 checksum.
-pub fn crc8(data: &[u8]) -> u8 {
+pub fn calculate(data: &[u8]) -> u8 {
     const CRC8_POLYNOMIAL: u8 = 0x31;
     let mut crc: u8 = 0xff;
     for byte in data {
@@ -15,15 +15,31 @@ pub fn crc8(data: &[u8]) -> u8 {
     crc
 }
 
+/// Iterate over the provided buffer and validate the CRC8 checksum.
+///
+/// If the checksum is wrong, return `Err`.
+///
+/// Note: This method will consider every third byte a checksum byte. If the buffer size is not a
+/// multiple of 3, then not all data will be validated.
+pub fn validate(buf: &[u8]) -> Result<(), ()> {
+    debug_assert!(buf.len() % 3 == 0, "Buffer must be a multiple of 3");
+    for chunk in buf.chunks(3) {
+        if chunk.len() == 3 && calculate(&[chunk[0], chunk[1]]) != chunk[2] {
+            return Err(());
+        }
+    }
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::crc;
 
-    /// Test the crc8 function against the test value provided in the
-    /// SHTC3 datasheet (section 5.10).
+    /// Test the crc function against the test value provided in the SHTC3 datasheet (section
+    /// 5.10).
     #[test]
     fn crc8_test_value() {
-        assert_eq!(crc8(&[0x00]), 0xac);
-        assert_eq!(crc8(&[0xbe, 0xef]), 0x92);
+        assert_eq!(crc::calculate(&[0x00]), 0xac);
+        assert_eq!(crc::calculate(&[0xbe, 0xef]), 0x92);
     }
 }
