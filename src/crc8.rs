3541c84cad1afbbf73b8a323c368338fa562ddc0
Rafael Bachmann <mediumendian@gmail.com>
2021-01-08 22:14:41 +0100

    Remove panicking behavior from crc8 module

--- src/crc8.rs
+++ src/crc8.rs
@@ -17,21 +17,27 @@ pub fn calculate(data: &[u8]) -> u8 {
     crc
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum Error {
+    WrongCrc,
+    WrongBufferSize,
+}
+
 /// Iterate over the provided buffer and validate the CRC8 checksums.
 ///
 /// The buffer must be in the form of `[d0, d1, crc01, d2, d3, crc23, ...]` where every third byte
 /// is the checksum byte of the previous two bytes
-/// If the checksum is wrong, return `Err`.
-///
-/// # Panics
+/// If any checksum is wrong, return `Error::WrongCrc`.
 ///
 /// This method will consider every third byte a checksum byte. If the buffer size is not a
-/// multiple of 3, then it will panic.
-pub fn validate(buf: &[u8]) -> Result<(), ()> {
-    assert!(buf.len() % 3 == 0, "Buffer must be a multiple of 3");
+/// multiple of 3, then it will return `Error::WrongBufferSize`
+pub fn validate(buf: &[u8]) -> Result<(), Error> {
+    if buf.len() % 3 != 0 {
+        return Err(Error::WrongBufferSize);
+    }
     for chunk in buf.chunks(3) {
         if calculate(&[chunk[0], chunk[1]]) != chunk[2] {
-            return Err(());
+            return Err(Error::WrongCrc);
         }
     }
     Ok(())
@@ -40,6 +46,7 @@ pub fn validate(buf: &[u8]) -> Result<(), ()> {
 #[cfg(test)]
 mod tests {
     use crate::crc8;
+    use crate::crc8::Error;
 
     /// Test the crc function against the test value provided in the SHTC3 datasheet (section
     /// 5.10).
@@ -50,22 +57,21 @@ mod tests {
     }
 
     #[test]
-    fn crc8_validate_empty() {
-        crc8::validate(&[]).unwrap();
+    fn crc8_validate_empty_ok() {
+        assert!(crc8::validate(&[]).is_ok());
     }
 
     #[test]
-    #[should_panic]
     fn crc8_validate_not_enough_data() {
-        crc8::validate(&[0xbe]).unwrap();
+        assert_eq!(Err(Error::WrongBufferSize), crc8::validate(&[0xbe]));
     }
 
     #[test]
     fn crc8_validate() {
         // Valid CRC
-        crc8::validate(&[0xbe, 0xef, 0x92]).unwrap();
+        assert!(crc8::validate(&[0xbe, 0xef, 0x92]).is_ok());
 
         // Invalid CRC
-        assert_eq!(crc8::validate(&[0xbe, 0xef, 0x91]), Err(()));
+        assert_eq!(crc8::validate(&[0xbe, 0xef, 0x91]), Err(Error::WrongCrc));
     }
 }
