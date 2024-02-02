388c2986a39aac3aa4bc8aac2fcbbefb69ca0af0
Lachezar Lechev <elpiel93@gmail.com>
2024-02-02 22:27:47 +0200

    feat: upgrade to embedded-hal@1.0
    Signed-off-by: Lachezar Lechev <elpiel93@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -20,12 +20,13 @@
 //! The I2C helpers work with any `embedded_hal::i2c` implementation.
 //!
 //! ```
-//! use embedded_hal_mock::i2c::{Mock as I2cMock, Transaction as I2cTransaction};
+//! use embedded_hal_mock::eh1::i2c::{Mock as I2cMock, Transaction as I2cTransaction};
 //! use sensirion_i2c::i2c;
 //!
 //! let expectations = [I2cTransaction::write(0x12, vec![0x34, 0x56])];
 //! let mut i2c_mock = I2cMock::new(&expectations);
 //! i2c::write_command_u16(&mut i2c_mock, 0x12, 0x3456);
+//! i2c_mock.done();
 //! ```
 
 #![deny(unsafe_code)]
